a3d0b6ddbf7d7351c82970139dd43d75077fd14a
Garry Hunt <44409417+officiallygarry@users.noreply.github.com>
2024-10-23 20:15:58 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,12 +11,12 @@ function custom_login_stylesheet() {
 add_action( 'login_enqueue_scripts', 'custom_login_stylesheet' );
 
 function my_login_logo_url() {
-    return 'https://web.officiallygarry.co.uk';
+    return 'https://officiallygarry.co.uk';
 }
 add_filter('login_headerurl', 'my_login_logo_url');
 
 function my_login_logo_url_title() {
-    return 'Digital content creator in Plymouth, Devon and Cornwall';
+    return 'WordPress and digital design specialist based in Plymouth, Devon and Cornwall.';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
